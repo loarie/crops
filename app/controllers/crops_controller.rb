5c970e65107b301350fe268c4ab8ded0375a95a6
Scott Loarie <loarie@gmail.com>
2012-07-24 06:51:30 -0700

    added a front end

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -1,44 +1,53 @@
 class CropsController < ApplicationController
-  
+  layout nil
+  layout 'application', :except => :index
   def index
     lat = ""
     lon = ""
+    @warning = false
     if params[:crop]
       crop = params[:crop]
     else
       @string = "Please enter a crop"
+      @warning = true
       return
     end
     if params[:lat]
       lat = params[:lat]
     else
       @string = "Please enter a latitude"
+      @warning = true
       return
     end
     if params[:lon]
       lon = params[:lon]
     else
       @string = "Please enter a longitude"
-      return
+      @warning = true
+      render :layout => false
     end
     if params[:stat]
       stat = params[:stat]
     else
       @string = "Please enter a statistic"
+      @warning = true
       return
     end
     crops = ["maize","soy","cassava"]
     unless crops.include? crop
       @string = "We have no projections for the crop #{crop}"
+      @warning = true
       return
     end
     unless /^[\d]+(\.[\d]+){0,1}$/ === lat && /^[\d]+(\.[\d]+){0,1}$/ === lon
       @string = "We could not match your location, Lat: #{lat} Lon: #{lon}"
+      @warning = true
       return
     end
     stats = ["planting","harvest","yield"]
     unless stats.include? stat
       @string = "We have no projections for #{stat}"
+      @warning = true
       return
     end    
     @string = "You requested #{stat} projections for #{crop} from Lat: #{lat} Lon: #{lon}"
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,10 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.root :controller => 'crops', :action => 'index'
+  map.root :controller => 'home', :action => 'index'
+  
+  map.resources :crops
+  map.resources :codes
   
   # See how all your routes lay out with "rake routes"
 
